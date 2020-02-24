7bd1a80677cf36b85b90a90aa172e3b6a7ff6afe
clannzu2 <clanzu2@yahoo.com>
2020-02-24 23:14:07 +0000

    added anchor

--- engine.py
+++ engine.py
@@ -6,7 +6,7 @@ import re
 
 for dirpath, dirname, filenames in os.walk("/home/clanzu2/Desktop/test"):
     for file in filenames:
-        if (re.match("\w+[.]css", file)):
+        if (re.match("^\w+[.]css$", file)):
             newFilarray = re.split("[.]", file)
             os.rename(os.path.join(dirpath, file), os.path.join(
                 dirpath, newFilarray[0] + ".module.css"))
